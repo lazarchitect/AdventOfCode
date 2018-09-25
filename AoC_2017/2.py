606302e51f51f0f8722d88b8a68f541f3e0afd73
Eddie <eddie.lazar@yahoo.com>
2018-09-25 17:28:41 -0400

    testing

---
+++ AoC_2017/2.py
@@ -0,0 +1,16 @@
+total = 0
+
+with open("2.txt") as f:
+	for line in f:
+		
+		arr = line.split("\t")
+		
+		for i in range(len(arr)):
+			arr[i] = int(arr[i])
+
+		biggest = max(arr)
+		smallest = min(arr)
+		diff = biggest - smallest
+		total+=diff
+
+print("Part 1:", total)
