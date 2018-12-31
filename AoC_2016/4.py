6948739a412b383e4766fa54e414c70babab41a5
Eddie <eddie.lazar@yahoo.com>
2018-12-31 02:19:59 -0500

    works in progress

--- AoC_2016/4.py
+++ AoC_2016/4.py
@@ -1,24 +1,29 @@
-for each in open("4.txt").readlines():
-	
-	ID = ""
-	
-	checksum = each[each.index("[")+1:each.index("]")]
-	
-	for i in range(len(each)):
-		if each[i].isdigit():
-			start = i
-			end = each.index("[")
-			ID = each[start:end]
-			break
-	
-	name = each[0:each.index(ID)].replace("-", "")
-	
-	letterFreqs = {}
-	for i in name:
-		if i in letterFreqs:
-			letterFreqs[i]+=1
+def isRealRoom(letters, checksum):
+	return letters == checksum
+
+def getLetters(name):
+	lettersDict = {}z
+	for character in name:
+		if character in lettersDict.keys():
+			lettersDict[character] += 1
+		else: 
+			lettersDict[character] = 1
+	for 		
+
+def extractName(roomcode):
+	return roomcode[0:firstNumeralIndex(roomcode) - 1].split("-").join("") 
+	#slices dont count the last one, so end on the separating dash. 
+	#and remove the hyphens.
+
+def firstNumeralIndex(roomcode):
+	x = 0
+	for i in roomcode:
+		if i.isdigit():
+			return x
 		else:
-			letterFreqs[i] =0
-			
-	print(letterFreqs)
-	exit()		
+			x += 1	
+
+
+
+for each in open("4.txt").readlines():
+	pass
